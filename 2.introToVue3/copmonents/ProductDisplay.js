6396b161538d692560b33993fc774f254c52d57a
Vladislav <machnevvlad@gmail.com>
2023-06-21 15:48:59 +0400

    Communicating Events

--- 2.introToVue3/copmonents/ProductDisplay.js
+++ 2.introToVue3/copmonents/ProductDisplay.js
@@ -27,7 +27,7 @@ app.component('product-display', {
                 <div
                     class="color-circle"
                     v-for="(variant, index) in variants" 
-                    :key="variant.id" 
+                    v-bind:key="variant.id" 
                     v-on:mouseover="updateVariant(index)"
                     v-bind:style="{ backgroundColor: variant.color}">
                 </div>
@@ -38,6 +38,14 @@ app.component('product-display', {
                     v-bind:disabled="!inStock"
                     v-on:click="addToCart">
                     Add to Cart
+                </button>
+
+                <button 
+                    class="button"
+                    v-bind:class="{ disabledButton: !inStock }"
+                    v-bind:disabled="!inStock"
+                    v-on:click="removeFromCart">
+                    Remove Item
                 </button>
             </div>
         </div>
@@ -56,7 +64,10 @@ app.component('product-display', {
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+        },
+        removeFromCart() {
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
         },
         updateVariant(index) {
             this.selectedVariant = index
