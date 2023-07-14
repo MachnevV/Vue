61b7769ed25bf927b67b79c430d026daf560c4f0
Vladislav <machnevvlad@gmail.com>
2023-07-14 19:06:54 +0400

    Communication Between Vue Components with Custom Events

--- 4. Components Fundamentals/plan-picker/app.js
+++ 4. Components Fundamentals/plan-picker/app.js
@@ -1,16 +1,28 @@
 let PlanComponent = {
     template: '#plan-template',
     props: {
-        name: {type: String, required: true}
-    
+        name: {type: String, required: true},
+        selected: {type: Boolean, default: false}    
+    },
+    methods: {
+        select(){
+            this.$emit('select', this.name)
+        }
     }
 }
+
 let PlanPickerComponent = {
     components: {plan: PlanComponent},
     template: '#plan-picker-template',
     data() {
         return {
-            plans: ['The Single', 'The Curious', 'The Addict']
+            plans: ['The Single', 'The Curious', 'The Addict'],
+            selectedPlan: null
+        }
+    },
+    methods: {
+        selectPlan(plan){
+            this.selectedPlan = plan
         }
     }
 }
