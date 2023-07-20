c48d0c41fa31d2cb2bcd716798d1a6b16bf5982f
Vladislav <machnevvlad@gmail.com>
2023-07-20 18:15:10 +0400

    Installing and Setting Up Vue Router with Vite

--- 5. VueRouter4/vue-travel-app/src/App.vue
+++ 5. VueRouter4/vue-travel-app/src/App.vue
@@ -10,6 +10,9 @@
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <router-link to="/">Home</router-link> | 
+  <router-link to="/about">About</router-link>
+  <router-view></router-view>
 </template>
 
 <style scoped>
