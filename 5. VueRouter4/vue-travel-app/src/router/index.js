6855297092e72feb40b1b05dd6f57cfe4be5d771
Vladislav <machnevvlad@gmail.com>
2023-07-21 18:47:47 +0400

    Clean Up

--- 5. VueRouter4/vue-travel-app/src/router/index.js
+++ 5. VueRouter4/vue-travel-app/src/router/index.js
@@ -3,11 +3,7 @@ import Home from '@/views/Home.vue'
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
-    {path: '/brazil', name: 'brazil', component: ()=>import('@/views/Brazil.vue')},
-    {path: '/hawaii', name: 'hawaii', component: ()=>import('@/views/Hawaii.vue')},
-    {path: '/jamaica', name: 'jamaica', component: ()=>import('@/views/Jamaica.vue')},
-    {path: '/panama', name: 'panama', component: ()=>import('@/views/Panama.vue')},
-    {path: '/vue-travel-app/:id', name:'destination.show', component: ()=>import('@/views/DestinationShow.vue')},
+    {path: '/vue-travel-app/:id/:slug', name:'destination.show', component: ()=>import('@/views/DestinationShow.vue')},
 
 ]
 
