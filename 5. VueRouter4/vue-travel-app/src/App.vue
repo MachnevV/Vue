72fea75d17a2d1463b4d6290972e1eb92188383d
Vladislav <machnevvlad@gmail.com>
2023-07-23 13:38:59 +0400

    Route Transitions(slide, moveUp, fade)

--- 5. VueRouter4/vue-travel-app/src/App.vue
+++ 5. VueRouter4/vue-travel-app/src/App.vue
@@ -1,13 +1,57 @@
+<template>
+  <TheNavigation/>
+  <div class="container">
+    <router-view v-slot="{Component}">
+      <!-- <transition name="slide" mode="out-in"> -->
+      <!-- <transition name="moveUp"> -->
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
+  </div>
+</template>
+
 <script>
 import TheNavigation from './components/TheNavigation.vue';
 export default{
   components: {TheNavigation}
 }
 </script>
+<style lang="css">
+  /* first css
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: opacity 1s, transform 1s;
+  }
+  .slide-enter-from,
+  .slide-leave-to {
+    opacity:0;
+    transform: translateX(-30%)
+  } */
 
-<template>
-  <TheNavigation/>
-  <div class="container">
-    <router-view :key="$route.path"></router-view>
-  </div>
-</template>
+  /* second css
+  .moveUp-enter-active {
+    animation: fadeIn 1s ease-in;
+  }
+  @keyframes fadeIn {
+    0% { opacity: 0; }
+    50% { opacity: 0.5; }
+    100% { opacity: 1; }
+  }
+  .moveUp-leave-active {
+    animation: moveUp 0.3s ease-in;
+  }
+  @keyframes moveUp {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(-400px); }
+  } */
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.3s;
+  }
+  .fade-enter,
+  .fade-leave-to {
+    opacity:0;
+  }
+</style>
