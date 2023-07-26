1411d4b26480b0fc09d2791ebd67a95fe3971622
Vladislav <machnevvlad@gmail.com>
2023-07-26 18:49:00 +0400

    Dynamic Routing

--- 5. VueRouter4/vue-travel-app/src/views/Home.vue
+++ 5. VueRouter4/vue-travel-app/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="home">
         <h1>All Destinations</h1>
+        <button @click="addDynamicRoute">Add Dynamic Route</button>
+        <router-link to="/dynamic">Visit Dynamic Route</router-link>
         <div class="destinations">
             <router-link 
                 v-for="destination in destinations"
@@ -15,12 +17,22 @@
 </template>
 <script>
 import sourceData from '@/data.json'
+import { isNavigationFailure, NavigationFailureType } from 'vue-router';
 export default {
     data(){
         return {
             destinations: sourceData.destinations
         }
+    },
+    methods:{
+        addDynamicRoute(){
+            this.$router.addRoute({
+                name: "dynamic",
+                path: "/dynamic",
+                component: () => import("@/views/Login.vue")
+            })
+            // this.$router.removeRoute("dynamic")
+        }
     }
-
-}
+};
 </script>
