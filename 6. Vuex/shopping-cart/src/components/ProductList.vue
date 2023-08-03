e342b8b70149d7cc6f074dd3533fbc78c3960041
Vladislav <machnevvlad@gmail.com>
2023-08-03 22:53:26 +0400

    Create a new project

---
+++ 6. Vuex/shopping-cart/src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul>
+            <h1>Product List</h1>
+            <li v-for="product in products">
+                {{product.title}} - {{product.price}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import shop from '../api/shop.js'
+
+    export default {
+        data () {
+            return {
+                products: []
+            }
+        },
+        created () {
+            shop.getProducts(products => {
+                this.products = products
+            })
+        }
+    }
+</script>
